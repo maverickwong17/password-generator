b63061f5163f68322c7377e72558cd406f173b9b
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-06-22 17:52:51 -0700

    generate password function output

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // Assignment Code
+
+//characters array
+var characters = {
+  upperCase : ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+  lowerCase : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  specialCase:['!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '`', '{', '|', '}', '~'],
+  number: ['1','2','3','4','5','6','7','8','9','0']
+}
+
+//Get references to the generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,5 +20,25 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//generatePassword function
+function generatePassword(){
+  var passlength = window.prompt("How many characters in your password?");
+  var conUpper
+  var intLength = parseInt(passlength);
+  var pending = 0
+  var complete = ""
+
+  for (var i=0; intLength > i; i++){
+    pending = "a";
+    complete = complete.concat(pending);
+    console.log(complete);
+  }
+
+  return complete;
+  }
+
+  //number, yes/no asnwers for password length and conditions
+  //number will be for looped and yes/no will be "if else" statements

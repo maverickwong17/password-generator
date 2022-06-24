744b6eeeac2f31019c2e0c5c1f5b57b4dd6271fc
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-06-23 21:03:05 -0700

    preliminary random generate code

--- script.js
+++ script.js
@@ -21,57 +21,72 @@ generateBtn.addEventListener("click", writePassword);
 var characters = {
   upperCase : ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
   lowerCase : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-  number: ['0','1','2','3','4','5','6','7','8','9'],
-  specialCase:['!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '`', '{', '|', '}', '~']
+  number : ['0','1','2','3','4','5','6','7','8','9'],
+  specialCase : ['!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '`', '{', '|', '}', '~'],
 }
 
 //generatePassword function
 function generatePassword(){
-  //character limit prompt
+  // character limit prompt
   var passlength = window.prompt("How many characters in your password?");
-  //reprompt if not within restricitons
+  // reprompt if not within restricitons
   while (passlength < 8 || passlength > 128){
     if (passlength == null) break;
     window.alert("Password must be between 8 and 128 characters long!");
-    passlength = window.prompt("How many characters in your password?");
-    console.log(passlength)
+    passlength =window.prompt("How many characters in your password?");
+    console.log(passlength);
   } 
-  
+// confirmation for included restrictions
 if (passlength == null){
 } else {
   var uppercon = confirm("Do you want to include uppercase?");
   var lowercon = confirm("Do you want to include lowercase?");
   var numcon = confirm("Do you want to include numbers?");
-  var specialcon = confirm("Do you want to include special characters?");}
-
+  var specialcon = confirm("Do you want to include special characters?");
   if(!uppercon && !lowercon && !numcon && !specialcon){
-    alert("You need to include at least one condition")
-  } 
-
-  console.log(uppercon)
-  console.log(lowercon)
-  console.log(numcon)
-  console.log(specialcon)
-
-  console.log(characters);
-
-
-  var pending = 0
-  var complete = ""
-
-  for (var i=0; i < passlength; i++){
-    pending = "a";
-    complete = complete + pending;
-    console.log(complete);
+      alert("You need to include at least one condition");
+    } 
   }
 
-  return complete;  //returns to writePassword function
+console.log(uppercon);
+console.log(lowercon);
+console.log(numcon);
+console.log(specialcon);
+
+var conArr = [uppercon , lowercon , numcon , specialcon]
+
+var useArr = Object.keys(characters);
+
+console.log(useArr);
+for (var i = 0; i < useArr.length - 1;i++){
+  console.log(useArr[i])
+}
+
+
+for (var i = useArr.length - 1; i >= 0; i--){
+  if (conArr[i] == 0){
+    useArr.splice(i,1)
   }
+}
+
+console.log(useArr);
+console.log(useArr[0][0]);
+
+var complete = ""
+for (var i=0; i < passlength; i++){
+  var pending = useArr[Math.floor(Math.random() *useArr.length)];
+  console.log(pending);
+  complete = complete + pending;
+  console.log(complete);
+}
+
+return complete;  //returns to writePassword function
+}
 
 
 //character limit prompt
 
 
 
-  //number, yes/no asnwers for password length and conditions
-  //number will be for looped and yes/no will be "if else" statement;
+//number, yes/no asnwers for password length and conditions
+//number will be for looped and yes/no will be "if else" statement;
